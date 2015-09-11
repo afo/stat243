622f0c61602301bbfb95f8ef7a4f226c601c7de0
Alexander Fred Ojala <afo@berkeley.edu>
2015-09-11 12:53:10 -0700

    Removed dots in info.r

--- info.r
+++ info.r
@@ -1,4 +1,4 @@
-name = "Alexander Fred Ojala".
-SID = "26958060".
-email = "afo@berkeley.edu".
-github_name = "alexanderfo".
+name = "Alexander Fred Ojala"
+SID = "26958060"
+email = "afo@berkeley.edu"
+github_name = "alexanderfo"
